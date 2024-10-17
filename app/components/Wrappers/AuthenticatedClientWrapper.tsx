bb148b3a5cfed0fd7f6918081224741ee366095e
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2024-10-17 16:29:43 +0900

    クライントコンポーネント用(react-query,Firebase auth使う)のラッパー

---
+++ app/components/Wrappers/AuthenticatedClientWrapper.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { redirect } from 'next/navigation'
+import { ReactNode, Suspense } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import { auth } from '@/app/lib/firebase/firebase-client'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { suspense: true } },
+})
+
+const C = ({ children }: { children: ReactNode }) => {
+  const [user, loading] = useAuthState(auth)
+
+  if (loading) return <>FirebaseAuth Loading...</>
+  if (!user) return redirect('/sign-in')
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </QueryClientProvider>
+  )
+}
+
+export default C
