f7bdfb2fd53efbda21b9036cdb66e3345bf949e5
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2024-10-17 16:31:15 +0900

    Update page.tsx

--- app/(firebase)/page.tsx
+++ app/(firebase)/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { SignOutButton } from '@/app/(firebase)'
 import { getCurrentUser } from '@/app/lib/firebase/firebase-admin'
 
@@ -5,9 +7,16 @@ const P = async () => {
   const currentUser = await getCurrentUser()
 
   return (
-    <div className='m-2'>
-      <h1 className='mb-2'>Authed Page</h1>
-      <p className='mb-2'>{currentUser.uid}</p>
+    <div className='flex flex-col gap-4 m-2'>
+      <div>
+        <h1 className='mb-2'>Authed Page</h1>
+        <p className='mb-2'>{currentUser.uid}</p>
+      </div>
+      <div>
+        <Link href='/reviews' className='underline text-blue-500'>
+          /reviews
+        </Link>
+      </div>
       <SignOutButton />
     </div>
   )
