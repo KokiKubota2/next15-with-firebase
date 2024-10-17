f1c37a1dd40d3cd95b4a082f102c09c8ec666747
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2024-10-17 16:28:27 +0900

    Update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,7 @@
 import clsx, { type ClassValue } from 'clsx'
+import { type DateTime } from 'luxon'
 import { twMerge } from 'tailwind-merge'
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export const formatTimestamp = (dt: DateTime) => dt.toFormat('yyyy-MM-dd HH:mm')
