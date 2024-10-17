09b0b426538b8ee260a0b66c8f0fe9e7f38ae57d
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2024-10-17 16:28:42 +0900

    etc....

--- app/types/addtional.d.ts
+++ app/types/addtional.d.ts
@@ -1,5 +1,16 @@
+export type QueryCondition = [string, WhereFilterOp, any]
+
+export type QueryOrder = { field: string; direction: OrderByDirection }
+
 export interface APIResponse<T> {
   success: boolean
   data?: T
   error?: string
+}
+
+export type Review = {
+  title: string
+  comment: string | null
+  createdAt: DateTime
+  createdBy: string
 }
